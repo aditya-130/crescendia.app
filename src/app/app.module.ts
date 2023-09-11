df38e041601fff9cda85c6d215a3b0fdd0573717
Aditya Pradeep <71806984+aditya-130@users.noreply.github.com>
2023-09-12 00:26:52 +0530

    App setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database'
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { environment } from 'src/environments/environment.development';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckOutComponent,
+    HomeComponent,
+    LoginComponent,
+    MyOrdersComponent,
+    OrderSuccessComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    AdminOrdersComponent,
+    AdminProductsComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
